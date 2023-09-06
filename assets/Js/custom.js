b1769760ce6968929cbdd7b19f0b33e6b2ad9bfa
suraj-4 <sson7563@gmail.com>
2023-09-06 22:58:06 +0530

    upload fianance files and foler

---
+++ assets/Js/custom.js
@@ -0,0 +1,127 @@
+const burgermenu = document.querySelector('.navbar-nav')
+$('#menu').click (function(){
+        this.classList.toggle("icon");
+        burgermenu.classList.toggle("showMenu");
+    }
+) 
+
+
+//Banner section
+$('.hero-slider').owlCarousel({
+    items:1,
+    loop: true,
+    margin:0,
+    nav:false,
+    dots:false,
+    autoplayHoverPause:true,
+    autoHeight:false,
+    autoplay :false,
+    autoplayTimeout :8000
+});
+
+// counter section
+$('.count').counterUp({
+    delay:20,
+    time:2000
+});
+
+// expert section
+$('.owl-two').owlCarousel({
+    items: 2,
+    margin: 30,
+    loop: true,
+    nav : true,
+    dots: false,
+    center : false,
+    autoplayHoverPause:true,
+    autoplay :false,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
+// testimonial section
+$('.owl-three').owlCarousel({
+    items: 2,
+    margin: 30,
+    loop: true,
+    nav : false,
+    dots: true,
+    center : false,
+    autoplayHoverPause:true,
+    autoplay :false,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
+// Latest News section
+$('.owl-forth').owlCarousel({
+    items: 3,
+    margin: 30,
+    loop: true,
+    autoplayHoverPause:true,
+    nav : false,
+    dots: true,
+    center : false,
+    autoplay :false,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+// our partner section
+$('.owl-fifth').owlCarousel({
+    items: 3,
+    margin: 30,
+    loop: true,
+    autoplayHoverPause:true,
+    nav : false,
+    dots: false,
+    center : false,
+    autoplay :false,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
